f4b7c84a8e19c1ac553b9b547c2f25b4261e9f58
Mosaab Gamal Kamel Abdelaleem <mosaabgamal0@gmail.com>
2021-12-29 20:53:02 +0200

    update

---
+++ background-scroll.js
@@ -0,0 +1,9 @@
+document.querySelector("body").onscroll = function myFunction() 
+{  
+    var scrolltotop = document.scrollingElement.scrollTop;
+    var target = document.querySelector("header");
+    var xvalue = "center";
+    var factor = -0.5;
+    var yvalue = scrolltotop * factor;
+    target.style.backgroundPosition = xvalue + " " + yvalue + "px";
+}
