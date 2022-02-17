8547618f21b005c7e26c580c6988d55f9b5dbbfe
Mosaab Gamal Kamel Abdelaleem <mosaabgamal0@gmail.com>
2022-02-17 05:20:20 +0200

    done but hume

--- background-scroll.js
+++
@@ -1,9 +0,0 @@
-document.querySelector("body").onscroll = function myFunction() 
-{  
-    var scrolltotop = document.scrollingElement.scrollTop;
-    var target = document.querySelector("header");
-    var xvalue = "center";
-    var factor = -0.5;
-    var yvalue = (scrolltotop + 840) * factor;
-    target.style.backgroundPosition = xvalue + " " + yvalue + "px";
-}
